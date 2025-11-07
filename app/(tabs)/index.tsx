c18940cacce662ce18637c243eef87fe753ff589
jessxth <jessica.thai@haw-hamburg.de>
2025-11-07 14:43:11 +0100

    text auskommentiert

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { Image } from 'expo-image';
 import React from 'react';
-import { Platform, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 
 import { HelloWave } from '@/components/hello-wave';
@@ -20,28 +20,28 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">WELCOME BACK!Hallo</ThemedText>
+        <ThemedText type="title">wer gibt essen aus ?</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        {/*<ThemedText type="subtitle">Step 1: Try it</ThemedText>*/}
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
+          {/*Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+          Press{' '}*/}
           <ThemedText type="defaultSemiBold">
-            {Platform.select({
+            {/*{Platform.select({
               ios: 'cmd + d',
               android: 'cmd + m',
               web: 'F12',
-            })}
+            })}*/}
           </ThemedText>{' '}
-          to open developer tools.
+          {/*to open developer tools.*/}
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <Link href="/modal">
           <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+            {/*<ThemedText type="subtitle">Step 2: Explore</ThemedText>*/}
           </Link.Trigger>
           <Link.Preview />
           <Link.Menu>
@@ -63,17 +63,17 @@ export default function HomeScreen() {
         </Link>
 
         <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
+          {/*{`Tap the Explore tab to learn more about what's included in this starter app.`}*/}
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+        {/*<ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>*/}
         <ThemedText>
-          {`When you're ready, run `}
+          {/*{`When you're ready, run `}
           <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
           <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
           <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+          <ThemedText type="defaultSemiBold">app-example</ThemedText>.*/}
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
